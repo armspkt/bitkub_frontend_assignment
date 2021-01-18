4d33bda5a96fa8563b8d74d0beb2fa5507a63fb1
ohmuay <67150143+ohmuay@users.noreply.github.com>
2021-01-19 02:23:48 +0700

    add info on welcomepage

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,17 @@
 import React from "react";
-
+import { FaFacebook, FaGithub } from "react-icons/fa";
 export default () => (
   <footer>
-    <p>copy right &copy; 2021 <cite>thana wisutphusi</cite></p>
+    <p>
+      copy right &copy; 2021 <cite>thana wisutphusi</cite>
+    </p>
+    <div id="info">
+      <a href="https://facebook.com/A.trbcp" target="blank">
+        <FaFacebook className="info__link fb" />
+      </a>
+      <a href="https://github.com/ohmuay" target="blank">
+        <FaGithub className="info__link git" />
+      </a>
+    </div>
   </footer>
 );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,11 @@
 import React from "react";
-
+import logo from "../img/logo.png";
 export default () => (
   <section id="home-page">
-    <h1 className="jumbo-text">Welcome</h1>
+    <h1 className="jumbo-text text-slim">Welcome</h1>
+      <h1 className="sub-text">
+        this is an assignment for Front-End Developer role at Bitkub Online
+        <a href="https://bitkub.com" target="blank"><img src={logo} /></a>
+      </h1>
   </section>
 );
